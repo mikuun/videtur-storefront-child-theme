252574f7623daf2dd2f2c8a1d4ce4700cec8301d
Mikaela Lidquist <95224154+mikuun@users.noreply.github.com>
2023-05-23 11:38:24 +0200

    New font and footer spacing
    Changed font to DM Sans and changed the footer spacing to match our new website.

--- functions.php
+++ functions.php
@@ -54,11 +54,7 @@ if ( ! function_exists( 'storefront_credit' ) ) {
     */
     function storefront_credit() {?>
     
-      <div class="site-info">
-    
-          <?php echo esc_html( '&copy; Videtur AB ' . ' ' . date( 'Y' ) ); ?>
-    
-        </div><!-- .site-info -->
+      
       <?php
       }
 }
@@ -178,10 +174,15 @@ add_filter( 'woocommerce_redirect_single_search_result', '__return_false' );
 add_action('wp_head', 'your_function_name');
 function your_function_name(){
 ?>
- <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
+ <!--<link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
  <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Italic.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
  <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Medium.woff2" as="font" type="font/woff2" crossorigin="anonymous">
  <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+-->
+<link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
+<link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Regular-Italic.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
+<link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
+<link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Bold-Italic.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
 
 <?php
 };
