4dcf3b6d11c3e050baff0af30fa8af2195416255
Mikaela Lidquist <95224154+mikuun@users.noreply.github.com>
2023-06-14 08:52:09 +0200

    Removed inactive fonts & added styling
    Removed inacitve font Berthold Akzidenz Grotesk
    
    Added styling for Filter Everything widgets

--- functions.php
+++ functions.php
@@ -174,11 +174,6 @@ add_filter( 'woocommerce_redirect_single_search_result', '__return_false' );
 add_action('wp_head', 'your_function_name');
 function your_function_name(){
 ?>
- <!--<link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
- <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Italic.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
- <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Medium.woff2" as="font" type="font/woff2" crossorigin="anonymous">
- <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/Berthold Akzidenz Grotesk Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous">
--->
 <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
 <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Regular-Italic.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
 <link rel="preload" href="/wp-content/themes/videtur-storefront-child-theme/assets/fonts/dm-sans/DMSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"> 
